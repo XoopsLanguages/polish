214c5e61811a4666c57c950c4f00f9579499a36a
mambax7 <mambax7@gmail.com>
2017-12-10 03:46:32 -0500

    2.5.9

---
+++ core/2.5.9/htdocs/language/polish/calendar.php
@@ -0,0 +1,72 @@
+<?php
+// $Id$
+// _LANGCODE: en
+// _CHARSET : UTF-8
+// Translator: XOOPS Translation Team
+// more examples: http://php.net/manual/en/function.date.php
+
+//%%%%%     Time Zone   %%%%
+define("_CAL_FORMAT","Y-m-d");
+define('_CAL_FORMAT',_SHORTDATESTRING);
+
+//%%%%%     JQuery Calendar Time Zone   %%%%
+define('_CAL_JQUERY_FORMAT',"yyyy-mm-dd");
+// Week Mini Text
+define('_CAL_MIN_SUNDAY',"Nd");
+define('_CAL_MIN_MONDAY',"Pn");
+define('_CAL_MIN_TUESDAY',"Wt");
+define('_CAL_MIN_WEDNESDAY',"Śr");
+define('_CAL_MIN_THURSDAY',"Cz");
+define('_CAL_MIN_FRIDAY',"Pt");
+define('_CAL_MIN_SATURDAY',"So");
+// Week Short Text
+define('_CAL_SHORT_SUNDAY',"Ndz");
+define('_CAL_SHORT_MONDAY',"Pon");
+define('_CAL_SHORT_TUESDAY',"Wt");
+define('_CAL_SHORT_WEDNESDAY',"Śr");
+define('_CAL_SHORT_THURSDAY',"Czw");
+define('_CAL_SHORT_FRIDAY',"Pt");
+define('_CAL_SHORT_SATURDAY',"Sob");
+define('_CAL_SHORT_JANUARY',"Sty");
+define('_CAL_SHORT_FEBRUARY',"Lut");
+define('_CAL_SHORT_MARCH',"Mar");
+define('_CAL_SHORT_APRIL',"Kwi");
+define('_CAL_SHORT_MAY',"Maj");
+define('_CAL_SHORT_JUNE',"Cze");
+define('_CAL_SHORT_JULY',"Lip");
+define('_CAL_SHORT_AUGUST',"Sie");
+define('_CAL_SHORT_SEPTEMBER',"Wrz");
+define('_CAL_SHORT_OCTOBER',"Paź");
+define('_CAL_SHORT_NOVEMBER',"Lis");
+define('_CAL_SHORT_DECEMBER',"Gru");
+
+define("_CAL_SUNDAY", "Niedziela");
+define("_CAL_MONDAY", "Poniedziałek");
+define("_CAL_TUESDAY", "Wtorek");
+define("_CAL_WEDNESDAY", "Środa");
+define("_CAL_THURSDAY", "Czwartek");
+define("_CAL_FRIDAY", "Piątek");
+define("_CAL_SATURDAY", "Sobota");
+define("_CAL_JANUARY", "Styczeń");
+define("_CAL_FEBRUARY", "Luty");
+define("_CAL_MARCH", "Marzec");
+define("_CAL_APRIL", "Kwiecień");
+define("_CAL_MAY", "Maj");
+define("_CAL_JUNE", "Czerwiec");
+define("_CAL_JULY", "Lipiec");
+define("_CAL_AUGUST", "Sierpień");
+define("_CAL_SEPTEMBER", "Wrzesień");
+define("_CAL_OCTOBER", "Październik");
+define("_CAL_NOVEMBER", "Listopad");
+define("_CAL_DECEMBER", "Grudzień");
+define("_CAL_TGL1STD", "Ustal pierwszy dzień tygodnia");
+define("_CAL_PREVYR", "Poprzedni rok (przytrzymaj kursor aby otrzymać menu)");
+define("_CAL_PREVMNTH", "Poprzedni miesiąc (przytrzymaj kursor aby otrzymać menu)");
+define("_CAL_GOTODAY", "Przejdź do dnia dzisiejszego");
+define("_CAL_NXTMNTH", "Następny miesiąc (przytrzymaj kursor aby otrzymać menu)");
+define("_CAL_NEXTYR", "Następny rok (przytrzymaj kursor aby otrzymać menu)");
+define("_CAL_SELDATE", "Wybierz datę");
+define("_CAL_DRAGMOVE", "Przeciągnij, aby przesunąć");
+define("_CAL_TODAY", "Dziś");
+define("_CAL_DISPM1ST", "Wyświetl Poniedziałek jako pierwszy");
+define("_CAL_DISPS1ST", "Wyświetl Niedzielę jako pierwszą");
